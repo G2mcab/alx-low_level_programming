05309872c2dc72ba07fac94c14d5ed43227abd62
G2mcab <abenezereliasacc@gmail.com>
2022-07-05 14:41:05 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks if the alphabet is capital
+ *@c: is int representing the char
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
