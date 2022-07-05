9f8562f75898822e92fe460a6ecbcfe7682cd1ad
G2mcab <abenezereliasacc@gmail.com>
2022-07-05 15:00:13 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value
+ *
+ * @n: input number
+ *
+ * Return: abs of number
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
