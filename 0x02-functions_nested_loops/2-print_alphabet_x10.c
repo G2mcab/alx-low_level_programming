6d60cc663b677924b85bd35c1814b83964c83b95
G2mcab <abenezereliasacc@gmail.com>
2022-07-05 14:04:39 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - func
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		char alph = 'a';
+
+		while (alph <= 'z')
+		{
+			_putchar (alph);
+			alph++;
+		}
+		_putchar ('\n');
+	}
+}
