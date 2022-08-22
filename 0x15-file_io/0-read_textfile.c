e007697bb276ee39f58b385971f5cd41316b8fb0
G2mcab <abenezereliasacc@gmail.com>
2022-08-22 04:20:36 -0700

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,36 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
+	int fd, num_to_print, written_num;
+	char *buffer;
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
 		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
-	{
-		free(buf);
+
+	/* return Number of bytes read on success */
+	num_to_print = read(fd, buffer, letters);
+	/* return Number of bytes written on success */
+	written_num = write(STDOUT_FILENO, buffer, num_to_print);
+
+	if (written_num < 0)
 		return (0);
-	}
-	buf[i] = '\0';
+
+	free(buffer);
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
+	return (num_to_print);
 }
