fec8a1b9a531fb533161f5a1596bd360def83b3c
G2mcab <abenezereliasacc@gmail.com>
2022-08-22 05:10:05 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fcntl.h>
 #include "main.h"
+#include <unistd.h>
+
 /**
- * read_textfile - reads a text file and prints it
- * to the POSIX standard output
- * @filename: name of the file
- * @letters: number of letters it should read and print
- * Return: actual number of letters it could read and print
+ * read_textfile - returns the actual number of letters it could read and print
+ * @filename: file to read and print
+ * @letters: number of letters to print
+ * Return: the number or letters
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, num_to_print, written_num;
-	char *buffer;
 
-	if (!filename)
+	int fd, sz;
+	char *buf;
+
+	if (filename == NULL)
 		return (0);
-
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	buffer = malloc(letters * sizeof(char));
-	if (!buffer)
+	sz = write(STDOUT_FILENO, buf, read(fd, buf, letters));
+	if (sz == -1)
 		return (0);
-
-	/* return Number of bytes read on success */
-	num_to_print = read(fd, buffer, letters);
-	/* return Number of bytes written on success */
-	written_num = write(STDOUT_FILENO, buffer, num_to_print);
-
-	if (written_num < 0)
-		return (0);
-
-	free(buffer);
 	close(fd);
-	return (num_to_print);
+	free(buf);
+	return (sz);
+
+
 }
